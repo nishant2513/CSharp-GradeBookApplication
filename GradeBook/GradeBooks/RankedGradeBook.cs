40154c3ed781cfd8fa5c564bee24714a20406ed0
Nishant Manchanda <nishant.manchanda17@gmail.com>
2019-04-08 11:23:53 -0700

    Item 4 in Project

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,6 +12,26 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {   
+            if(Students.Count() <5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             
@@ -28,7 +48,7 @@ namespace GradeBook.GradeBooks
                 grades.Reverse();
             }
            
-           // var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+           
 
             if (grades[(threshold * 1) - 1] <= averageGrade)
                 return 'A';
